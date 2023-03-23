74915cfb8c3accb19bfccd80ec08ac1c59dd253e
Dmitry Kudin <dim271096@gmail.com>
2023-03-23 21:35:03 +0100

    Adjust Dockerfiles, use old-style Program class for Migrator

--- FamilyAssistant.Migrator/Program.cs
+++ FamilyAssistant.Migrator/Program.cs
@@ -1,19 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
 
-using FamilyAssistant.Migrator;
-using Microsoft.Extensions.Configuration;
+namespace FamilyAssistant.Migrator;
 
-var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-var appSettingsName = string.IsNullOrEmpty(environment)
-    ? "appsettings.json"
-    : $"appsettings.{environment}.json";
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var appSettingsName = string.IsNullOrEmpty(environment)
+            ? "appsettings.json"
+            : $"appsettings.{environment}.json";
 
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile(appSettingsName)
-    .Build();
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(appSettingsName)
+            .Build();
 
-var connectionString = configuration["DbSettings:ConnectionString"];
-var migrationRunner = new MigrationRunner(connectionString);
+        var connectionString = configuration["DbSettings:ConnectionString"];
+        var migrationRunner = new MigrationRunner(connectionString);
 
-migrationRunner.MigrateDatabase();
+        migrationRunner.MigrateDatabase();
+    }
+}
+
